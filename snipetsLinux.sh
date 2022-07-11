d66b929e1ef9665b790842bd8df7c1220a49afdb
David León Vilca <datevid@gmail.com>
2022-07-10 23:53:06 -0500

    Update snipetsLinux.sh

--- snipetsLinux.sh
+++ snipetsLinux.sh
@@ -1,2 +1,16 @@
 #desbloquear el control de seguridad/unblock control security anydesk linux
 anydesk-global-settings
+
+# install elementary desktop
+sudo add-apt-repository ppa:elementary-os/stable
+sudo add-apt-repository ppa:elementary-os/daily
+sudo apt-get update
+sudo apt-get install elementary-desktop
+
+
+# uninstall elementary desktop
+sudo apt autoremove --purge elementary-desktop
+
+
+
+
